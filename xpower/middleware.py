67712ba12681bf4a2e106631556d15288c143ce0
Paul Michali <pc@michali.net>
2023-12-04 15:02:49 -0500

    Changed middleware for new Django syntax, recreated new example.

--- xpower/middleware.py
+++ xpower/middleware.py
@@ -4,7 +4,19 @@ from django.conf import settings
 
 HEADER = getattr(settings, 'X_POWERED_BY', 'Django/%(version)s') % {'version': get_version()}
 
-class XPoweredByMiddleware(object):
-    def process_response(self, request, response):
+class XPoweredByMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
         response['X-Powered-By'] = HEADER
+
         return response
